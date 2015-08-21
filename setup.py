6c0022b8c22db4f2b0be70d19e7291d80c30578e
bumblebee <git@bumblebee.cc>
2015-08-21 09:18:27 +0200

    Add setup.py to install as a module.

---
+++ setup.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='cts2000',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=['pyserial'],
+    zip_safe=False,
+    author='Bumblebee',
+    author_email='git@bumblebee.cc',
+    url='https://github.com/bumblebeefr/pyCTS2000',
+    license='GNU GENERAL PUBLIC LICENSE Version 3',
+)
